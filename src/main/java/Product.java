1ef526665470c7bdcd672c4450f79bc434a36b0d
amuica <Piticotu_1>
2019-06-10 11:03:08 +0300

    Initial Amuica

---
+++ src/main/java/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    private String name;
+    private Integer price;
+
+    public Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+}
